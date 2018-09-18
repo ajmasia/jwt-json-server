9b6e8904142dc008d3142cc9a1541f2246b3ca3f
amas1i <antonio.masia@unitedcuisines.com>
2018-09-18 14:20:47 +0200

    Auth server implemented

---
+++ api_v1/server.js
@@ -0,0 +1,89 @@
+/**
+ * Require necessary libraries
+ */
+const fs = require('fs')
+const bodyParser = require('body-parser')
+const jsonServer = require('json-server')
+const jwt = require('jsonwebtoken')
+
+// Create server
+const server = jsonServer.create()
+
+// Create router
+const touter = jsonServer.router('./db.json')
+
+// Users database
+const usersDB = JSON.parse(fs.readFile('./users.json', 'UTF8'))
+
+// Default middlewares
+server.use(jsonServer.defaults())
+
+// JWT confing data
+const SECRET_KEY = '123456789'
+const expiresIn = '1h'
+
+// Create a token from a payload
+function createToken(payload) {
+  return jwt.sign(payload, SECRET_KEY, { expiresIn })
+}
+
+// Verify the token
+function verifyToken(token) {
+  return jwt.verify(
+    token,
+    SECRET_KEY,
+    (err, decode) => (decode !== undefined ? decode : err)
+  )
+}
+
+// Check if the user exists in database
+function isAuthenticated({ email, password }) {
+  return (
+    userdb.users.findIndex(
+      user => user.email === email && user.password === password
+    ) !== -1
+  )
+}
+
+/**
+ * Method: POST
+ * Endpoint: /auth/login
+ */
+server.post('/auth/login', (req, res) => {
+  const { email, password } = req.body
+  if (isAuthenticated({ email, password }) === false) {
+    const status = 401
+    const message = 'Incorrect email or password'
+    res.status(status).json({ status, message })
+    return
+  }
+  const access_token = createToken({ email, password })
+  res.status(200).json({ access_token })
+})
+
+// Chech auth middleware
+server.use(/^(?!\/auth).*$/, (req, res, next) => {
+  if (
+    req.headers.authorization === undefined ||
+    req.headers.authorization.split(' ')[0] !== 'Bearer'
+  ) {
+    const status = 401
+    const message = 'Bad authorization header'
+    res.status(status).json({ status, message })
+    return
+  }
+  try {
+    verifyToken(req.headers.authorization.split(' ')[1])
+    next()
+  } catch (err) {
+    const status = 401
+    const message = 'Error: access_token is not valid'
+    res.status(status).json({ status, message })
+  }
+})
+
+// Server mount
+server.use(router)
+server.listen(3000, () => {
+  console.log('Auth API server runing on port 3000 ...')
+})
